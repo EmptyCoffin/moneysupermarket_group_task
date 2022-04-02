bdf5124a57970b09a301e3b6f9ca69ad826f096f
EmptyCoffin <ji-hayes@hotmail.com>
2022-04-02 17:07:30 +0100

    Added unit tests for product base

---
+++ BasketUnitTests/Products/ProductBaseTests.cs
@@ -0,0 +1,41 @@
+using BasketUnitTests.TestProducts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasketUnitTests.Products
+{
+    [TestClass]
+    public class ProductBaseTests
+    {
+        [TestMethod]
+        public void Price_GivenNoDiscount_ShouldReturnOriginalPrice()
+        {
+            // arrange
+            var product = new TestProduct1();
+
+            // act
+            var result = product.Price;
+
+            // assert
+            Assert.AreEqual(product.OriginalPrice, result);
+        }
+
+        [DataTestMethod]
+        [DataRow(30, 2.45)]
+        [DataRow(100, 0)]
+        [DataRow(0, 3.50)]
+        public void Price_GivenDiscount_ShouldReturnDiscountedPrice(double discount, double expectedValue)
+        {
+            // arrange
+            var product = new TestProduct1
+            {
+                Dicount = discount
+            };
+
+            // act
+            var result = product.Price;
+
+            // assert
+            Assert.AreEqual(expectedValue, result);
+        }
+    }
+}
